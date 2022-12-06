8014e8af2d34c6987eb191fecc7eee2b425f9ad6
JoaoAmorim01 <joaovrosa10@gmail.com>
2022-12-06 10:34:13 -0300

    Te Amo

---
+++ love.py
@@ -0,0 +1,7 @@
+amor_da_minha_vida = {
+    "Thamize": {"pixo_no_seu_nome":"Da Vila Sônia a Jabaquara só pra ver sua cara, te amo amor da minha vida, por você minhas lata de tinta o Cristo Redentos rabiscaria"}
+}
+myEternalLove = (amor_da_minha_vida.values())
+teAmo = "Thamize" in amor_da_minha_vida
+print(myEternalLove)
+print(teAmo)
